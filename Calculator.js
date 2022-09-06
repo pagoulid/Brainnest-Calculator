c6b6fb6aaaf3f19cd9c33e3c555ef176925bd179
pagoulid <pagoulid@gmail.com>
2022-09-06 18:17:46 +0300

    adding operators to DOM

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,27 @@
 
 const OPERATORS = {'+':0,'-':1,'*':2,'/':3};
+const clearHandler = ()=>{
+    let calcScreen = document.querySelector('.calc-display');
+    let digitNodes = document.querySelectorAll('h3');
+    digitNodes.forEach((digitNode)=>{
+        calcScreen.removeChild(digitNode);
+    });
+}
+const DispDigitHandler=(element)=>{
+    console.log(element.textContent);
+    let calcScreen = document.querySelector('.calc-display');
+    let digitNode = document.createElement('h3');
+    digitNode.setAttribute('style','display:inline-block;')
+    digitNode.textContent=element.textContent;
+    calcScreen.appendChild(digitNode);
+}
+let digits=document.querySelectorAll('.digit');
+let clear = document.querySelector('.clear');
+clear.addEventListener('click',clearHandler)
+digits.forEach((digit)=>{
+    
+    digit.addEventListener('click',()=>{DispDigitHandler(digit)})
+});
 function add(num1,num2){return num1+num2;}
 function subtract(num1,num2){return num1-num2;}
 function multiply(num1,num2){return num1*num2;}
