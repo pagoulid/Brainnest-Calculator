e5981286d8908da846b525683f4228f7be4211bf
pagoulid <pagoulid@gmail.com>
2022-09-07 14:10:33 +0300

    Cleaning code...

--- Calculator.js
+++ Calculator.js
@@ -1,96 +1,49 @@
 
 const OPERATORS = {'+':0,'-':1,'*':2,'/':3};
-const STORED_NUMBER_VALS = [];
-const STORED_OPERATORS = []
-const setCalcResult = (answer)=>{
-    let calcScreen = document.querySelector('.calc-display');
-    let answerNode = document.createElement('h3');
-    answerNode.setAttribute('style','display:inline-block;')
-    answerNode.textContent=answer;
-    calcScreen.appendChild(answerNode);
 
+const setCalcResult = (dispNode,answer)=>{
+    dispNode.textContent=answer;
 }
 const clearHandler = ()=>{
-    let calcScreen = document.querySelector('.calc-display');
-    let digitNodes = document.querySelectorAll('h3');
-    digitNodes.forEach((digitNode)=>{
-        calcScreen.removeChild(digitNode);
-    });
+    let symbolNode = document.querySelector('h3');
+    symbolNode.textContent='';
 }
-const DispDigitHandler=(element)=>{
-    
-    let calcScreen = document.querySelector('.calc-display');
-    let digitNode = document.createElement('h3');
-    digitNode.setAttribute('style','display:inline-block;')
-    digitNode.textContent=element.textContent;
-    calcScreen.appendChild(digitNode);
+const DisplayHandler=(element)=>{
+    let symbolNode = document.querySelector('h3');
+    symbolNode.textContent+=element.textContent.trim();
 }
 const resultHandler = ()=>{
-    let symbols =[];
-    let strNum = '';
-    let symbolNodes = document.querySelectorAll('h3');
-    /** first part : append all elements to array */
-    for(let i = 0;i<symbolNodes.length;i++){/*concat all the digits before an operator to form a number*/ 
-        let symbolContent = symbolNodes[i].textContent.trim();/* push everything in an array */
-        
-        if(symbolContent=='+'||symbolContent=='-'||symbolContent=='*'||symbolContent=='/'){
-            symbols.push(parseInt(strNum));
-            
-            strNum='';
-            symbols.push(symbolContent);
-            
-
-        }
-        else{
-            strNum+=symbolContent;
-            
-        }
-
-
-    }
     
-    symbols.push(parseInt(strNum));/*store last element*/
-    console.log(symbols);
-    /** first part : append all elements to array */
-    /**second part */
-    for(let k = 0;k<symbols.length;k++){
-        if(k==0){
-            prevVal=symbols[k];
-            
-            
-            
-        }
-        else{
-            if(symbols[k]=='+'||symbols[k]=='-'||symbols[k]=='*'||symbols[k]=='/'){
-                prevVal=operate(prevVal,symbols[k+1],symbols[k]);
-                console.log(prevVal);
-
-            }
-    }
-}
- 
-    /**second part */
+    let strNum = '';
+    let symbolStrNode = document.querySelector('h3');
+    let symbols = symbolStrNode.textContent.split('');/*'12+25*34'->['1','2','+','2','5','*','3','4']*/ 
+    
+    let calcArray=generateSymbolArr(symbols);/* ['1','2','+','2','5','*','3','4'] -> [12,'+',25,'*',34]*/ 
+    let calcResult = calculations(calcArray);/*calculations result(number)*/ 
     
     clearHandler();/* clear old memory */
-    setCalcResult(prevVal);
-
-    
+    setCalcResult(symbolStrNode,calcResult);   
 
 }
+/* MAIN */
 let digits=document.querySelectorAll('.digit');
 let operators=document.querySelectorAll('.operator');
 let equals = document.querySelector('.equals');
 let clear = document.querySelector('.clear');
+
 clear.addEventListener('click',clearHandler);
 equals.addEventListener('click',resultHandler);
+
 digits.forEach((digit)=>{
     
-    digit.addEventListener('click',()=>{DispDigitHandler(digit)});
+    digit.addEventListener('click',()=>{DisplayHandler(digit)});
 });
 operators.forEach((operator)=>{
     
-    operator.addEventListener('click',()=>{DispDigitHandler(operator)});
+    operator.addEventListener('click',()=>{DisplayHandler(operator)});
 });
+/* MAIN */
+
 function add(num1,num2){return num1+num2;}
 function subtract(num1,num2){return num1-num2;}
 function multiply(num1,num2){return num1*num2;}
@@ -113,5 +66,35 @@ function operate(num1,num2,op){
             break;
     }
     return answer;
+
+}
+
+function generateSymbolArr(array){
+    let strNum = '';
+    let newArr = [];
+    for(let i=0;i<array.length;i++){
+
+        if(array[i]=='+'||array[i]=='-'||array[i]=='*'||array[i]=='/'){
+            newArr.push(parseInt(strNum));
+            strNum='';
+            newArr.push(array[i]);
+        }
+        else{
+            strNum+=array[i];    
+        } 
+    }
+    newArr.push(parseInt(strNum));
+    return newArr;
+
+
+}
+function calculations(symbolArr){ /*Take arr of nums and ops -> return calc result*/ 
+    let prevVal=symbolArr[0];
+    for(let k=0;k<symbolArr.length;k++){
+        if(symbolArr[k]=='+'||symbolArr[k]=='-'||symbolArr[k]=='*'||symbolArr[k]=='/'){
+            prevVal=operate(prevVal,symbolArr[k+1],symbolArr[k]);
+        }
+    }
+    return prevVal;
 
 }
